17ccd1d050d076de715a1c4c6e560c902e169f0b
Sébastien Chopin <seb@nuxtjs.com>
2020-10-07 12:36:14 +0200

    chore: add seo meta tags

--- website/layouts/default.vue
+++ website/layouts/default.vue
@@ -7,7 +7,14 @@ export default {
   head: {
     bodyAttrs: {
       class: 'bg-cloudy-grey min-h-screen'
-    }
+    },
+    link: [
+      { rel: 'shortcut icon', href: '/favicon.ico' }
+    ],
+    meta: [
+      { hid: 'charset', charset: 'utf-8' },
+      { hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ]
   }  
 }
 </script>
