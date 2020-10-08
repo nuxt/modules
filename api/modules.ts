d7c0f4a4c1e9d21e1500137fcd8263666f399d10
Sébastien Chopin <seb@nuxtjs.com>
2020-10-08 15:15:41 +0200

    chore: update api

--- api/modules.ts
+++ api/modules.ts
@@ -40,8 +40,6 @@ const handler = async (req, res) => {
 const allowCors = fn => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
     'Access-Control-Allow-Headers',
