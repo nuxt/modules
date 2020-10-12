6bfdbf20128d9d88134345e2587e1239e99ad6f1
Sébastien Chopin <seb@nuxtjs.com>
2020-10-12 12:32:07 +0200

    chore: update design

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,9 +27,6 @@ export default {
     bodyAttrs: {
       class: 'bg-cloudy-grey min-h-screen'
     },
-    link: [
-      { rel: 'shortcut icon', href: '/favicon.ico' }
-    ],
     meta: [
       { hid: 'charset', charset: 'utf-8' },
       { hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1' }
