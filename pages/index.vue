0d2dd28606b74f2fdbb52558e52b4a770d53fc92
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2020-10-13 09:41:57 +0200

    feat: add link to modules docs

--- pages/index.vue
+++ pages/index.vue
@@ -111,14 +111,17 @@
     </div>
 
     <!-- Footer -->
-    <div class="container mx-auto flex justify-center px-4 sm:px-0 pt-12 text-stone-green items-center space-x-2">
-      <a href="https://vercel.com" rel=noopener target="_blank" aria-label="go to vercel">
-        <IconVercel alt="Vercel" />
-      </a>
-      <a href="https://nuxtjs.org" rel=noopener target="_blank" aria-label="go to nuxt">
-        <IconWithNuxt alt="Nuxt" />
-      </a>
-    </div>
+    <footer class="container mx-auto flex flex-col justify-center pt-12 text-stone-green items-center">
+      <p>For more information on Nuxt modules, including how to create a module, check out our <a href="https://nuxtjs.org/guides/directory-structure/modules" rel="noopener" target="_blank" class="text-md leading-4 items-center space-x-1 text-grey border-b border-stone-green hover:text-green-500 hover:border-green-600">docs</a></p>
+      <div class="flex justify-center px-4 sm:px-0 pt-6 space-x-2">
+        <a href="https://vercel.com" rel=noopener target="_blank" aria-label="go to vercel">
+          <IconVercel alt="Vercel" />
+        </a>
+        <a href="https://nuxtjs.org" rel=noopener target="_blank" aria-label="go to nuxt">
+          <IconWithNuxt alt="Nuxt" />
+        </a>
+      </div>
+    </footer>
   </div>
 </template>
 
