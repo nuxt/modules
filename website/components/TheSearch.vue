235abb515df02cdce51ca4745d56e26832fa8bf4
productdevbook <38668796+productdevbook@users.noreply.github.com>
2021-11-03 20:43:03 +0300

    improve theSearch

---
+++ website/components/TheSearch.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex items-center justify-between w-full container mx-auto px-4 sm:px-0 py-4">
+    <IconNuxt alt="Nuxt" />
+    <slot></slot>
+    <iconMoon />
+  </div>
+</template>
