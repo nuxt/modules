d6c529f822594eb20785901e18a2a4f44a000b83
Daniel Roe <daniel@roe.dev>
2022-07-20 21:52:23 +0100

    fix: use `app.head` for metadata

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -6,32 +6,34 @@ const description = 'Discover our list of modules to supercharge your Nuxt proje
 const url = 'https://modules.nuxtjs.org'
 
 export default defineNuxtConfig({
-  meta: {
-    bodyAttrs: {
-      class: 'min-h-screen'
-    },
-    title,
-    meta: [
-      { hid: 'charset', charset: 'utf-8' },
-      { hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: description },
-      { hid: 'og:site_name', property: 'og:site_name', content: title },
-      { hid: 'og:description', property: 'og:description', content: description },
-      { hid: 'og:type', property: 'og:type', content: 'website' },
-      { hid: 'og:url', property: 'og:url', content: url },
-      { hid: 'og:image', property: 'og:image', content: `${url}/preview.png` },
-      // Twitter Card
-      { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
-      { hid: 'twitter:site', name: 'twitter:site', content: '@nuxt_js' },
-      { hid: 'twitter:title', name: 'twitter:title', content: title },
-      { hid: 'twitter:description', name: 'twitter:description', content: description },
-      { hid: 'twitter:image', name: 'twitter:image', content: `${url}/preview.png` },
-      { hid: 'twitter:image:alt', name: 'twitter:image:alt', content: title }
-    ],
-    link: [
-      { hid: 'favicon', rel: 'icon', type: 'image/png', href: '/icon.png' },
-      { hid: 'search', rel: 'search', type: 'application/opensearchdescription+xml', href: '/search.xml', title: 'Nuxt Modules' }
-    ]
+  app: {
+    head: {
+      bodyAttrs: {
+        class: 'min-h-screen'
+      },
+      title,
+      meta: [
+        { hid: 'charset', charset: 'utf-8' },
+        { hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: description },
+        { hid: 'og:site_name', property: 'og:site_name', content: title },
+        { hid: 'og:description', property: 'og:description', content: description },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:url', property: 'og:url', content: url },
+        { hid: 'og:image', property: 'og:image', content: `${url}/preview.png` },
+        // Twitter Card
+        { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+        { hid: 'twitter:site', name: 'twitter:site', content: '@nuxt_js' },
+        { hid: 'twitter:title', name: 'twitter:title', content: title },
+        { hid: 'twitter:description', name: 'twitter:description', content: description },
+        { hid: 'twitter:image', name: 'twitter:image', content: `${url}/preview.png` },
+        { hid: 'twitter:image:alt', name: 'twitter:image:alt', content: title }
+      ],
+      link: [
+        { hid: 'favicon', rel: 'icon', type: 'image/png', href: '/icon.png' },
+        { hid: 'search', rel: 'search', type: 'application/opensearchdescription+xml', href: '/search.xml', title: 'Nuxt Modules' }
+      ]
+    }
   },
   components: true,
   modules: [
