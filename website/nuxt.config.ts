b956319b1bde7ee118aa91b52c5621fc2dcfe70c
Anthony Fu <anthonyfu117@hotmail.com>
2022-07-21 14:47:14 +0800

    fix: `floating-vue` version

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -70,15 +70,5 @@ export default defineNuxtConfig({
   },
   plausible: {
     domain: 'modules.nuxtjs.org'
-  },
-  vue: {
-    compilerOptions: {
-      directiveTransforms: {
-        tooltip: () => ({
-          props: [],
-          needRuntime: true
-        })
-      }
-    }
   }
 })
